612b4b431b793a9cbe563267b7d54c8d3887cc38
mnguyen-se <minhnguyen08112004@gmail.com>
2026-01-24 11:51:45 +0700

    BE - [UPDATE] update module controller

--- BE/src/main/java/com/example/online_learning/controller/ModuleController.java
+++ BE/src/main/java/com/example/online_learning/controller/ModuleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/modules")
+@RequestMapping("/api/v1/modules")
 public class ModuleController {
 
     private final ModuleService moduleService;
