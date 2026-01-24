a5237ac1d96037660d043ce7c6abf9ecba312458
Nguyễn Văn Long <Longnvse180646@fpt.edu.vn>
2026-01-24 13:23:51 +0700

    fix enrollment

--- BE/src/main/java/com/example/online_learning/repository/EnrollmentRepository.java
+++ BE/src/main/java/com/example/online_learning/repository/EnrollmentRepository.java
@@ -4,6 +4,7 @@ import com.example.online_learning.entity.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -19,4 +20,10 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     Optional<Enrollment> findByUser_UserIdAndCourse_CourseId(
             Long userId, Long courseId
     );
+    
+    List<Enrollment> findByCourse_CourseIdAndDeletedFalse(Long courseId);
+    
+    List<Enrollment> findByUser_UserIdAndDeletedFalse(Long userId);
+    
+    List<Enrollment> findByUser_UserNameAndDeletedFalse(String username);
 }
--- BE/src/main/java/com/example/online_learning/service/EnrollmentService.java
+++ BE/src/main/java/com/example/online_learning/service/EnrollmentService.java
@@ -1,8 +1,16 @@
 package com.example.online_learning.service;
 
 import com.example.online_learning.dto.request.EnrollStudentDtoReq;
+import com.example.online_learning.dto.response.EnrolledCourseDtoRes;
+import com.example.online_learning.dto.response.EnrolledStudentDtoRes;
+import com.example.online_learning.security.CustomUserDetail;
+
+import java.util.List;
 
 public interface EnrollmentService {
     void enrollStudent(EnrollStudentDtoReq request);
     void unenrollStudent(Long courseId, String username);
+    List<EnrolledStudentDtoRes> getEnrolledStudentsByCourseId(Long courseId);
+    List<EnrolledCourseDtoRes> getEnrolledCoursesByUsername(String username);
+    List<EnrolledCourseDtoRes> getMyEnrolledCourses(CustomUserDetail userDetail);
 }
