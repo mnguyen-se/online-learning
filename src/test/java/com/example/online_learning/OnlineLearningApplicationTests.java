c7158555448afbdcfd84932ed34ff7b3a2ea823f
mnguyen-se <minhnguyen08112004@gmail.com>
2026-02-10 16:58:48 +0700

    BE - [UPDATE] update test

--- src/test/java/com/example/online_learning/OnlineLearningApplicationTests.java
+++ src/test/java/com/example/online_learning/OnlineLearningApplicationTests.java
@@ -1,25 +1,29 @@
 package com.example.online_learning;
 
 import com.cloudinary.Cloudinary;
+import com.example.online_learning.security.JwtFilter;
 import com.example.online_learning.security.JwtUtil;
 import com.example.online_learning.service.UserDetailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
 @ActiveProfiles("test")
 class OnlineLearningApplicationTests {
+	@MockBean JwtUtil jwtUtil;
+	@MockBean JwtFilter jwtFilter;
+	@MockBean UserDetailService userDetailService;
 	@MockBean
-	private JwtUtil jwtUtil;
-
+	AuthenticationManager authenticationManager;
 	@MockBean
-	private Cloudinary cloudinary;
-
+	PasswordEncoder passwordEncoder;
 	@MockBean
-	private UserDetailService userDetailService;
+	Cloudinary cloudinary;
 
 	@Test
 	void contextLoads() {
