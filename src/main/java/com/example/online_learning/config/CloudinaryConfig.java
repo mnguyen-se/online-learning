c8340e038ba1e08cbe8daa5546e2a047df49f073
mnguyen-se <minhnguyen08112004@gmail.com>
2026-02-10 16:43:59 +0700

    BE - [UPDATE] update application-test.yml

--- src/main/java/com/example/online_learning/config/CloudinaryConfig.java
+++ src/main/java/com/example/online_learning/config/CloudinaryConfig.java
@@ -4,10 +4,11 @@ import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@Profile("!test")
 @Configuration
 public class CloudinaryConfig {
     @Value("${spring.cloudinary.cloud-name}")
--- src/test/java/com/example/online_learning/OnlineLearningApplicationTests.java
+++ src/test/java/com/example/online_learning/OnlineLearningApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.online_learning;
 
+import com.cloudinary.Cloudinary;
+import com.example.online_learning.security.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
 @ActiveProfiles("test")
 class OnlineLearningApplicationTests {
+	@MockBean
+	private JwtUtil jwtUtil;
+
+	@MockBean
+	private Cloudinary cloudinary;
 
 	@Test
 	void contextLoads() {
