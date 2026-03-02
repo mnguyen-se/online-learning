05d85a4dbb484dc384190f25cd254d9112550001
mnguyen-se <minhnguyen08112004@gmail.com>
2026-03-02 22:01:41 +0700

    BE - [UPDATE] update redis auto reset when restrart app

--- src/main/java/com/example/online_learning/config/RedisConfig.java
+++ src/main/java/com/example/online_learning/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.example.online_learning.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -37,6 +38,14 @@ public class RedisConfig {
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
                 .build();
+    }
+
+    @Bean
+    public ApplicationRunner clearRedisOnStartup(RedisConnectionFactory factory) {
+        return args -> {
+            factory.getConnection().serverCommands().flushDb(); // chỉ clear DB hiện tại
+            System.out.println("🔥 Redis cache cleared on startup!");
+        };
     }
 
 }
