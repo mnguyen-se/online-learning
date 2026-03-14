2520643000fcbc42b0709252f9bb820e1ec08a1f
mnguyen-se <minhnguyen08112004@gmail.com>
2026-03-14 11:46:13 +0700

    BE - [UPDATE] update application-test.yaml

--- src/main/java/com/example/online_learning/config/RedisConfig.java
+++ src/main/java/com/example/online_learning/config/RedisConfig.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
+@Profile("!test")
 public class RedisConfig {
 
     @Bean
